a7ce065e48c80758a5bc5fae716220d828eddd0a
Johanna Amann <johanna@icir.org>
2017-08-11 17:35:27 -0700

    Remove annoying fasd bugging.

--- plugins/available/fasd.plugin.bash
+++ plugins/available/fasd.plugin.bash
@@ -4,8 +4,7 @@ about-plugin 'initialize fasd (see https://github.com/clvv/fasd)'
 __init_fasd() {
   command -v fasd &> /dev/null
   if [ $? -eq 1 ]; then
-    echo -e "You must install fasd before you can use this plugin"
-    echo -e "See: https://github.com/clvv/fasd"
+		:
   else
     eval "$(fasd --init posix-alias)"
 
